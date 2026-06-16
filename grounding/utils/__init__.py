77927a0ad6e0dd9849b6b89b90b7ce8b7e166c91
jizheng_fei <jizheng@um.com>
2026-06-16 22:04:34 +0800

    Restore grounding utils compatibility imports

---
+++ grounding/utils/__init__.py
@@ -0,0 +1,3 @@
+"""Compatibility exports for legacy grounding utilities."""
+
+from grounding.legacy.utils.utils import *  # noqa: F401,F403
---
+++ grounding/utils/utils.py
@@ -0,0 +1,3 @@
+"""Compatibility shim for callers still importing grounding.utils.utils."""
+
+from grounding.legacy.utils.utils import *  # noqa: F401,F403
